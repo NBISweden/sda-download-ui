ac073056d8bec78e61c9fd892640f717c7acd630
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2026-03-30 12:40:15 +0200

    chore: adding boilerplate of next with bootstrap and bootstrap icons issue #17

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <div  className="d-flex justify-content-center align-items-center flex-column mt-5">
+          <h1>Welcome to SDA download UI!</h1>
+            <p>The CLI you know, with a UI you’ll love.</p>
+            <p>Making great tools more approachable.</p>
+            <p className="fst-italic">From commands to clicks.</p>
+            <a
+                href="https://github.com/NBISweden/sda-download-ui"
+                className="btn btn-primary btn-lg"
+                rel="noopener noreferrer"
+            >
+                <i className="bi bi-github"></i> Go to repository
+            </a>
+        </div>
+      </main>
+    </div>
+  );
+}
