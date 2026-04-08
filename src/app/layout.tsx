b26e10af90d8b54146bab6d972ea8fe1178c0c72
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2026-04-08 09:30:36 +0200

    feat: formating files in the project- issue #7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import './globals.css';
-
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "SDA Download UI",
@@ -15,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" >
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
