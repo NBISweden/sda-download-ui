8b2a38df1e13a1cab55db50127aa3dfae40a2e9b
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2026-04-20 10:55:04 +0200

    fix: set session length to equal the input token

--- frontend/src/app/lib/session.ts
+++ frontend/src/app/lib/session.ts
@@ -5,7 +5,14 @@ import { getSessionManager, SessionData } from "./SessionManager";
 
 export async function createOrUpdateSession(data: SessionData) {
   const sessionManager = await getSessionManager();
-  return await sessionManager.createOrUpdateSession(data);
+  const tokenInfo = await getClaims(data.token);
+  const now = new Date().getTime() / 1000;
+  const sessionLength = Math.max(_getOrDefault(tokenInfo.exp, now) - now, 0);
+  return await sessionManager.createOrUpdateSession(data, sessionLength * 1000);
+}
+
+function _getOrDefault<T>(value: T | undefined, defaultValue: T): T {
+  return value === undefined ? defaultValue : value;
 }
 
 export async function getSession(): Promise<SessionData | null> {
