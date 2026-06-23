31ce2ce15dfb117dcd02da92df0dd2392a4bac07
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2026-06-23 16:35:55 +0200

    feat: separating components #119

---
+++ frontend/src/app/components/ModalDialog.tsx
@@ -0,0 +1,55 @@
+type ModalProps = {
+  id: string;
+  title: string;
+  body: string;
+  action: () => void;
+  iconClass: string;
+  actionButtonLabel: string;
+};
+export function ModalDialog({ id, title, body, action, iconClass, actionButtonLabel }: ModalProps) {
+  const titleId = `${id}Label`;
+  return (
+    <>
+      <div
+        className="modal fade"
+        id={id}
+        tabIndex="-1"
+        aria-labelledby={titleId}
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id={titleId}>
+                {title}
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">{body}</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={action}
+              >
+                <i className={`bi ${iconClass} me-1`}></i>{actionButtonLabel}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
