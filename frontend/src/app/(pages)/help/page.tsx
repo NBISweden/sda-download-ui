6ca6fedb1b7dbebd43df8c7965541d56885e22b0
Henrike <henrike.wiemker@nbis.se>
2026-09-01 11:35:28 +0200

    feat: add help page, subheadings and nav link. Issue #162

---
+++ frontend/src/app/(pages)/help/page.tsx
@@ -0,0 +1,18 @@
+export default function HelpPage() {
+
+  return (
+    <main>
+      <div className="container">
+        <h2 className="my-3">How to use the this download service</h2>
+        <h3>Before you start</h3>
+        <h3>Login</h3>
+        <h3>View datasets</h3>
+        <h3>Explore a dataset</h3>
+        <h3>Upload a public key for downloading</h3>
+        <h3>Download files</h3>
+        <h3>Download a full dataset</h3>
+        <h3>Download checksums</h3>
+      </div>
+    </main>
+  )
+}
