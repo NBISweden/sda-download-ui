1f73859bada3da7b488938b0aacee0e70c15fc4f
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2026-09-08 16:16:11 +0200

    feat: added select option instead of buttons issue #205

--- frontend/src/app/components/ItemsPerPage.tsx
+++ frontend/src/app/components/ItemsPerPage.tsx
@@ -15,30 +15,32 @@ export function useItemsPerPage(
   };
 }
 
-export function ItemSelector<T extends string | number>({
+export function ItemSelector({
   item,
   items,
   setItem,
   label,
 }: {
-  item: T;
-  items: T[];
-  setItem: (i: T) => void;
+  item: number;
+  items: number[];
+  setItem: (i: number) => void;
   label: string;
 }) {
   return (
     <div className="col-12 d-flex mb-3">
       <div className="btn-group flex-grow-0">
-        {items.map((i) => (
-          <a
-            key={i}
-            onClick={() => setItem(i)}
-            className={`btn btn-secondary ${i === item ? "active" : ""}`}
-            aria-current="page"
-          >
-            {i}
-          </a>
-        ))}
+        <select
+            className="form-select"
+            aria-label={label}
+            value={item}
+            onChange={(event) => setItem(Number(event.target.value))}
+        >
+          {items.map((i) => (
+              <option key={i} value={i}>
+                {i}
+              </option>
+          ))}
+        </select>
       </div>
       <span className="py-2 mx-2 ms-lg-4 ms-3 flex-shrink-0 flex-grow-1">
         {label}
