3620e935fab61b6f45a817e99ff177686cde2651
Henrike <henrike.wiemker@nbis.se>
2026-09-08 12:36:52 +0200

    feat: change select files button color. Issue #206

--- frontend/src/app/components/DatasetFiles.tsx
+++ frontend/src/app/components/DatasetFiles.tsx
@@ -209,7 +209,7 @@ export default function DatasetFiles({
         <div className="d-flex gap-2">
           <button
             type="button"
-            className="btn btn-secondary selection-button"
+            className="btn btn-outline-primary selection-button"
             onClick={handleSelectionButtonClick}
             disabled={currentFiles.length === 0}
           >
